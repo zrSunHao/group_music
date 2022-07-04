f4b8de25a78d063c8c15e47be600b98fb4097b7c
sunhao <2080680175@qq.com>
2022-07-04 21:35:45 +0800

    search page layout.

--- Hao.GroupMusic.App/Pages/SearchPage.xaml.cs
+++ Hao.GroupMusic.App/Pages/SearchPage.xaml.cs
@@ -1,3 +1,5 @@
+using Hao.GroupMusic.App.Business.ViewModels;
+
 namespace Hao.GroupMusic.App.Pages;
 
 public partial class SearchPage : ContentPage
@@ -5,5 +7,7 @@ public partial class SearchPage : ContentPage
 	public SearchPage()
 	{
 		InitializeComponent();
-	}
+		this.BindingContext = new SearchPageViewModel();
+
+    }
 }
---
+++ Hao.GroupMusic.App/Styles/CollectListStyles.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hao.GroupMusic.App.Styles;
+
+public partial class CollectListStyles : ResourceDictionary
+{
+	public CollectListStyles()
+	{
+		InitializeComponent();
+	}
+}
